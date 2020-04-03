182e54af2725f7be77cf3e7c8bda2e49006ac864
intromisan <42156539+intromisan@users.noreply.github.com>
2020-04-03 21:28:44 +0200

    JS 2
    Added popup window

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
+//Pushes a popup on reload
+const popup = document.getElementById("popupWelcome");
+const btnClose = document.getElementById("close");
+const pushPopup = () => {
+  popup.style.display = 'block';
+}
+
+btnClose.addEventListener('click', () => {
+  popup.style.display = "none";
+});
+
+const checkFirstVisit = () => {
+  if (document.cookie.split(';').some((item) => item.includes('test=1'))) {
+    pushPopup();
+  } else {
+    document.cookie = 'test=1';
+  }
+}
+
+window.addEventListener('click', (event) => {
+  if (event.target == popup) {
+    popup.style.display = "none";
+    document.cookie = 'test=2';
+  }
+});
+
+// Slider animation
 const issues = document.getElementById('issues');
 const issueSlider = document.getElementById('issueSlider');
 const issueIcon = document.getElementById('issueIcon');
