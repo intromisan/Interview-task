44979a78c8760e5cb3c5ad5949f7eca2d5fe4722
intromisan <42156539+intromisan@users.noreply.github.com>
2020-04-04 18:06:40 +0200

    Mobile - animation, pop-up
    Finalised pop-up

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-//Pushes a popup on reload
+//Pushes a popup on 2nd reload, if clicked X or screen stops pop-ups
 const popup = document.getElementById("popupWelcome");
 const btnClose = document.getElementById("close");
 const pushPopup = () => {
   popup.style.display = 'block';
 }
 
-btnClose.addEventListener('click', () => {
-  popup.style.display = "none";
-});
+// if (document.cookie.split(';').some((item) => item.includes('test=1')) && document.cookie.split(';').some((item) => item.includes('test=2')))
 
 const checkFirstVisit = () => {
-  if (document.cookie.split(';').some((item) => item.includes('test=1'))) {
-    pushPopup();
+  if (document.cookie.split(';').some((item) => item.includes('test=1'))){
+    if (document.cookie.split(';').some((item) => item.includes('test=1')) && document.cookie.split(';').some((item) => item.includes('enter=2'))){
+      return true
+    } else {
+      pushPopup();
+    }
   } else {
     document.cookie = 'test=1';
   }
 }
 
+btnClose.addEventListener('click', () => {
+  popup.style.display = "none";
+  document.cookie = 'enter=2';
+});
+
 window.addEventListener('click', (event) => {
   if (event.target == popup) {
     popup.style.display = "none";
-    document.cookie = 'test=2';
+    document.cookie = 'enter=2';
   }
 });
 
